76f60b09c3c340508ea813861841309dd0d6e212
oraman <oraman@naver.com>
2022-11-29 12:41:59 +0900

    Welcome 파일이 merge 된후 다시 수정되었습니다.

--- src/ch02/Welcome.java
+++ src/ch02/Welcome.java
@@ -4,5 +4,7 @@ public class Welcome {
     public static void main(String[] args) {
         System.out.println("welcome01");
         System.out.println("welcome02_written by hee");
+        System.out.println("welcome02");
+        System.out.println("welcome03");
     }
 }
